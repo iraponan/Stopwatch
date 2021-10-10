40259c9b6db9eea2d0e781500daf026409300041
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2021-10-10 16:31:29 -0300

    Opções do Menu

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -9,11 +9,14 @@ namespace Stopwatch {
 
         static void Menu() {
             Console.Clear();
-            Console.WriteLine("S = Segundos");
-            Console.WriteLine("M = Minutos");
-            Console.WriteLine("H = Horas");
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minutos => 1m = 1 minuto");
+            Console.WriteLine("H = Horas => 1h = 1 hora");
             Console.WriteLine("0 = sair");
-            Console.Write("Quanto tempo deseja contar? ");
+            Console.WriteLine("Quanto tempo deseja contar? ");
+
+            string opcao = Console.ReadLine().ToLower();
+
         }
 
         static void Start(int time) {
